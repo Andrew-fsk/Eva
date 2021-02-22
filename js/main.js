22cd09b26ac47a9552b5f2e7aff75566a0234db4
Andrew <feshykandrew.work@gmail.com>
2021-02-22 21:06:23 +0200

    header|banner

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-
+    var typed = new Typed('.banner-typed', {
+        strings: ["КРАСИВО", "СЛАДКО", "ГОРЯЧО", "НЕЖНО"],
+        typeSpeed: 120,
+        loop: true,
+        loopCount: Infinity,
+        backDelay: 700,
+    });
 })
+
+function modalToggle() {
+    $('.modal').toggleClass('hide');
+    $('.back-layer').toggleClass('hide');
+    $('body').toggleClass('no-scroll');
+}
